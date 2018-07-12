94d0ecf153da168f016d7591058dde85804a0534
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-13 00:07:42 +0800

    当输入格式正常时，对check()进行测试

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -4,11 +4,17 @@ package tw.core;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tw.core.exception.AnswerFormatIncorrectException;
+import tw.core.model.Record;
 
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -39,16 +45,31 @@ public class AnswerTest {
 
 
     @Test
-    public void should_throw_Excepetion_when_num_greater_than_9() {
+    public void should_throw_Excepetion_when_num_greater_than_9() throws Exception{
         Answer answer=Answer.createAnswer("1 2 3 10");
         try{
             answer.validate();
+            fail("Answer format is incorrect");
 
         }catch (AnswerFormatIncorrectException  e){
-            return;
+
 
         }
-        fail("输入的数字大于9,应该抛异常");
+    }
+
+
+    @Test
+    public void should_throw_Excepetion_when_num_repear(){
+        Answer answer=Answer.createAnswer("1 2 3 3");
+        try{
+            answer.validate();
+            fail("Answer format is incorrect");
+
+        }catch (AnswerFormatIncorrectException  e){
+
+
+        }
+
     }
 
     @Test
@@ -56,13 +77,32 @@ public class AnswerTest {
         Answer answer=Answer.createAnswer("1 2、 3 4");
         try{
             answer.validate();
+            fail("Answer format is incorrect");
 
         }catch (Exception e){
-            return;
+
 
         }
-        fail("输入错误格式,应该抛异常");
     }
+
+
+    @Test
+    public void should_return_true_value_when_given_correct_input(){
+        //given
+        Answer answerString = mock(Answer.class);
+        //涉及到answer类的其他参数
+        //when
+        when(answerString.getIndexOfNum("1")).thenReturn(0);
+        when(answerString.getIndexOfNum("2")).thenReturn(1);
+        when(answerString.getIndexOfNum("3")).thenReturn(2);
+        when(answerString.getIndexOfNum("4")).thenReturn(3);
+        //then
+        Record record = actualAnswer.check(answerString);
+        assertThat(record.getValue(), is("4A0B"));
+
+    }
+
+
 
 
 
