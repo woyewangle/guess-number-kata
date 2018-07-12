f4ae55bea26552a7cf45071f1d474d2470dce772
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-13 00:53:59 +0800

    当输入含有其他字符时的测试用例

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -5,6 +5,7 @@ import tw.validator.InputValidator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/5/17.
@@ -52,5 +53,31 @@ public class InputValidatorTest {
         Boolean isValidated = inputValidator.validate(num);
         //then
         assertThat(isValidated, is(false));
+    }
+
+    @Test
+    public void should_return_false_when_given_a_Four_digits_and_contains_special_char() throws Exception {
+        //given
+        InputValidator inputValidator = new InputValidator();
+        String num = "1, 2, 3, 4";
+        //whens
+        Boolean isValidated = inputValidator.validate(num);
+        //then
+        assertThat(isValidated, is(false));
+    }
+
+    @Test
+    public void should_get_the_FAIL_status_when_guess_input_is_wrong() throws Exception {
+        InputValidator inputValidator = new InputValidator();
+        String input = "1, 2, 3, 4";
+
+        try {
+            boolean result = inputValidator.validate(input);
+            fail("输入格式错误，此处该抛出输入非法的异常");
+
+        }catch (Exception e){
+
+        }
+
     }
 }
